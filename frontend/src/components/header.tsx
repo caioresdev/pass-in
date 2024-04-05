03ea67b3a6a432958121e0ec479d5de8f2101a2a
Paijas <matheuspaigel2016@gmail.com>
2024-04-04 22:02:15 -0300

    Update: Componente Header finalizado

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,12 +1,12 @@
 import nlwUniteIcon from '../assets/nlw_unite_icon.svg'
 export  function Header() {
   return (
-    <div className=''>
+    <div className='flex items-center gap-5 py-2'>
         <img src={nlwUniteIcon}  />
 
-        <nav>
-          <a href="">Eventos</a>
-          <a href="">Participantes</a>
+        <nav className='flex items-center gap-5'>
+          <a href="" className='text-sm font-medium text-zinc-300'>Eventos</a>
+          <a href="" className='text-sm font-medium'>Participantes</a>
         </nav>
     </div>
 
